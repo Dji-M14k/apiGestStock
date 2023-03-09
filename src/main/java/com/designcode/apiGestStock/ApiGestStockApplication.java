922ee9f057bc05974e6cdd5a3508f58b6e096d58
Jean-Marie <douetaijm@gmail.com>
2023-03-09 14:30:50 +0000

    first commit

---
+++ src/main/java/com/designcode/apiGestStock/ApiGestStockApplication.java
@@ -0,0 +1,13 @@
+package com.designcode.apiGestStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designcode/apiGestStock/ApiGestStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designcode.apiGestStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
